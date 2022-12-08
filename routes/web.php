583aa02968e2003512450f58077932b7cf240187
neoko <33611354+almasz243@users.noreply.github.com>
2022-12-08 17:28:17 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -19,10 +19,10 @@ Route::get('/', function () {
 });
 Route::get('/bot', [\App\Http\Controllers\botcontroller::class, 'botControl'])->name('botControl');
 Route::post('/5716304295:AAHVDPCzodAQOwQU5G-7kLfRUU7AVa2VTRg/webhook', function () {
-    $updates = Telegram::getWebhookUpdates();
+    $update = json_decode($_POST['update']);
     $data = [
         'chat_id' => '864640107',
-        'text' => $updates->getBody()
+        'text' => 'ok'
     ];
     $response = file_get_contents("https://api.telegram.org/bot5716304295:AAHVDPCzodAQOwQU5G-7kLfRUU7AVa2VTRg/sendMessage?" . http_build_query($data));
 });

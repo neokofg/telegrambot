7fa7bd6bea94aec4f445cc360cc819643c2b30f7
neoko <33611354+almasz243@users.noreply.github.com>
2022-12-08 15:28:09 +0300

    Update botcontroller.php

--- app/Http/Controllers/botcontroller.php
+++ app/Http/Controllers/botcontroller.php
@@ -13,10 +13,10 @@ class botcontroller extends Controller
         $result = $response->request($method, $route, $params);
         return(string) $result->getBody();
     }
-    public function botResponse(Request $request){
-        $id = $_POST['id'];
+    public function botResponse(){
+        $update = json_decode(file_get_contents('php://input'));
         $data = [
-            'chat_id' => $id,
+            'chat_id' => '864640107',
             'text' => 'Hi there!'
         ];
         $response = file_get_contents("https://api.telegram.org/bot5716304295:AAHVDPCzodAQOwQU5G-7kLfRUU7AVa2VTRg/sendMessage?" . http_build_query($data));

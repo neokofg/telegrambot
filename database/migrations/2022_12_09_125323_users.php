e99745ef43beecbbd6a3abb943d7fceb264f69cd
neoko <33611354+almasz243@users.noreply.github.com>
2022-12-09 15:56:59 +0300

    db

--- database/migrations/2022_12_09_125323_users.php
+++ database/migrations/2022_12_09_125323_users.php
@@ -15,11 +15,15 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('userid')->unique();
+            $table->string('chatid')->unique();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('username');
+            $table->string('language_code');
+            $table->string('status');
+            $table->string('isstart');
+            $table->string('passport');
             $table->timestamps();
         });
     }

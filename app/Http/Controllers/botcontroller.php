dd414b0ca78099e1f6b99eb3ee5f63e991fb8ea5
neoko <33611354+almasz243@users.noreply.github.com>
2022-12-09 17:46:36 +0300

    Update botcontroller.php

--- app/Http/Controllers/botcontroller.php
+++ app/Http/Controllers/botcontroller.php
@@ -209,6 +209,13 @@ class botcontroller extends Controller
                         ];
                         $response = Http::get("https://api.telegram.org/bot5716304295:AAHVDPCzodAQOwQU5G-7kLfRUU7AVa2VTRg/sendMessage?" . http_build_query($data));
                     }else if($userItem->status == 'phoneadvertclaim'){
+                        $userdata = array(
+                            'status' => 'advertclaimall',
+                            'phone' => $update->message->text,
+                            "updated_at" => date('Y-m-d H:i:s')
+                        );
+                        DB::table('users')->where('userid','=',$update->message->from->id)->update($userdata);
+                    }else if($userItem->status == 'advertclaimall'){
                         $keyboard =
                             '{
                                 "inline_keyboard": [[
@@ -223,12 +230,6 @@ class botcontroller extends Controller
                                 ]
                             }';
                         $decode = json_decode($keyboard);
-                        $userdata = array(
-                            'status' => 'advertclaimall',
-                            'phone' => $update->message->text,
-                            "updated_at" => date('Y-m-d H:i:s')
-                        );
-                        DB::table('users')->where('userid','=',$update->message->from->id)->update($userdata);
                         $data = [
                             'chat_id' => $update->message->chat->id,
                             'text' => 'Вы хотите взять с собой посылку

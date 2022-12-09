ceacab779c0b78d0c91236ef4759b49292d07190
neoko <33611354+almasz243@users.noreply.github.com>
2022-12-09 15:34:11 +0300

    Update botcontroller.php

--- app/Http/Controllers/botcontroller.php
+++ app/Http/Controllers/botcontroller.php
@@ -46,40 +46,42 @@ class botcontroller extends Controller
             }
 
         }
-        $decode = json_decode($keyboard);
-        if($update->message->text == '/start'){
-            $data = [
-                'chat_id' => $update->message->chat->id,
-                'reply_to_message_id' => $update->message->message_id,
-                'text' => 'Все мы попадали в ситуацию, когда срочно нужно отправить документы в другой город. Почтой России очень долго, другими службами дорого, а ещё нужно, чтобы документы прибыли на следующий день. Особенно в нынешнее неспокойное время.
+        if (isset($update->message)) {
+            $decode = json_decode($keyboard);
+            if($update->message->text == '/start'){
+                $data = [
+                    'chat_id' => $update->message->chat->id,
+                    'reply_to_message_id' => $update->message->message_id,
+                    'text' => 'Все мы попадали в ситуацию, когда срочно нужно отправить документы в другой город. Почтой России очень долго, другими службами дорого, а ещё нужно, чтобы документы прибыли на следующий день. Особенно в нынешнее неспокойное время.
 
 Для этого и был создан канал GETLET https://t.me/getlet, в первую очередь для взаимопомощи между земляками из Республики Саха (Якутия), а также как возможность получить дополнительный заработок'
-            ];
-            $response = Http::get("https://api.telegram.org/bot5716304295:AAHVDPCzodAQOwQU5G-7kLfRUU7AVa2VTRg/sendMessage?" . http_build_query($data));
-            $data2 = [
-                'chat_id' => $update->message->chat->id,
-                'text' => 'Что вы хотите сделать?',
-                'reply_markup' => json_encode($decode)
-            ];
-            $response = Http::get("https://api.telegram.org/bot5716304295:AAHVDPCzodAQOwQU5G-7kLfRUU7AVa2VTRg/sendMessage?" . http_build_query($data2));
-        }else if($update->message->text == '/help'){
-            $data = [
-                'chat_id' => $update->message->chat->id,
-                'text' => 'Извините что я вас выебал'
-            ];
-            $response = Http::get("https://api.telegram.org/bot5716304295:AAHVDPCzodAQOwQU5G-7kLfRUU7AVa2VTRg/sendMessage?" . http_build_query($data));
-        }else if($update->message->text == 'K4N') {
-            $data = [
-                'chat_id' => $update->message->chat->id,
-                'text' => 'КАНЧА'
-            ];
-            $response = Http::get("https://api.telegram.org/bot5716304295:AAHVDPCzodAQOwQU5G-7kLfRUU7AVa2VTRg/sendMessage?" . http_build_query($data));
-        }else{
-            $data = [
-                'chat_id' => $update->message->chat->id,
-                'text' => 'Я вас не понял'
-            ];
-            $response = Http::get("https://api.telegram.org/bot5716304295:AAHVDPCzodAQOwQU5G-7kLfRUU7AVa2VTRg/sendMessage?" . http_build_query($data));
+                ];
+                $response = Http::get("https://api.telegram.org/bot5716304295:AAHVDPCzodAQOwQU5G-7kLfRUU7AVa2VTRg/sendMessage?" . http_build_query($data));
+                $data2 = [
+                    'chat_id' => $update->message->chat->id,
+                    'text' => 'Что вы хотите сделать?',
+                    'reply_markup' => json_encode($decode)
+                ];
+                $response = Http::get("https://api.telegram.org/bot5716304295:AAHVDPCzodAQOwQU5G-7kLfRUU7AVa2VTRg/sendMessage?" . http_build_query($data2));
+            }else if($update->message->text == '/help'){
+                $data = [
+                    'chat_id' => $update->message->chat->id,
+                    'text' => 'Извините что я вас выебал'
+                ];
+                $response = Http::get("https://api.telegram.org/bot5716304295:AAHVDPCzodAQOwQU5G-7kLfRUU7AVa2VTRg/sendMessage?" . http_build_query($data));
+            }else if($update->message->text == 'K4N') {
+                $data = [
+                    'chat_id' => $update->message->chat->id,
+                    'text' => 'КАНЧА'
+                ];
+                $response = Http::get("https://api.telegram.org/bot5716304295:AAHVDPCzodAQOwQU5G-7kLfRUU7AVa2VTRg/sendMessage?" . http_build_query($data));
+            }else{
+                $data = [
+                    'chat_id' => $update->message->chat->id,
+                    'text' => 'Я вас не понял'
+                ];
+                $response = Http::get("https://api.telegram.org/bot5716304295:AAHVDPCzodAQOwQU5G-7kLfRUU7AVa2VTRg/sendMessage?" . http_build_query($data));
+            }
         }
     }
 }

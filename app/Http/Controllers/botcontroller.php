09febfda12ddc0d72c46c252e03afb70008cc616
neoko <33611354+almasz243@users.noreply.github.com>
2022-12-10 19:18:33 +0300

    Update botcontroller.php

--- app/Http/Controllers/botcontroller.php
+++ app/Http/Controllers/botcontroller.php
@@ -6,6 +6,7 @@ use GuzzleHttp\Client;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Str;
 use SebastianBergmann\CodeCoverage\Report\PHP;
 

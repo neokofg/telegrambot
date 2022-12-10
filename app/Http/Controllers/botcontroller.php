49d45aa8c3d4f8a4babcfc40021899f6fe6c2a96
neoko <33611354+almasz243@users.noreply.github.com>
2022-12-10 19:41:34 +0300

    Update botcontroller.php

--- app/Http/Controllers/botcontroller.php
+++ app/Http/Controllers/botcontroller.php
@@ -276,14 +276,7 @@ class botcontroller extends Controller
                         $validator = Validator::make($input, [
                             'weight' => 'starts_with:1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50|ends_with:кг'
                         ]);
-                        if ($validator->fails()) {
-                            $data = [
-                                'chat_id' => $update->message->chat->id,
-                                'text' => 'Введите вес в кг!'.PHP_EOL.'Формат: вес кг',
-                                'reply_to_message_id' => $update->message->message_id,
-                            ];
-                            $response = Http::get("https://api.telegram.org/bot5716304295:AAHVDPCzodAQOwQU5G-7kLfRUU7AVa2VTRg/sendMessage?" . http_build_query($data));
-                        }else if($update->message->text == 0 OR $update->message->text == '0 кг'){
+                        if($update->message->text == 0 OR $update->message->text == '0 кг'){
                             $userdata = array(
                                 'status' => 'itemadvertclaim',
                                 'weight' => 'Документы',
@@ -293,6 +286,13 @@ class botcontroller extends Controller
                             $data = [
                                 'chat_id' => $update->message->chat->id,
                                 'text' => 'Что можете взять с собой?'.PHP_EOL.'Пример: документы, мелкие посылки, багаж',
+                                'reply_to_message_id' => $update->message->message_id,
+                            ];
+                            $response = Http::get("https://api.telegram.org/bot5716304295:AAHVDPCzodAQOwQU5G-7kLfRUU7AVa2VTRg/sendMessage?" . http_build_query($data));
+                        }else if ($validator->fails()) {
+                            $data = [
+                                'chat_id' => $update->message->chat->id,
+                                'text' => 'Введите вес в кг!'.PHP_EOL.'Формат: вес кг',
                                 'reply_to_message_id' => $update->message->message_id,
                             ];
                             $response = Http::get("https://api.telegram.org/bot5716304295:AAHVDPCzodAQOwQU5G-7kLfRUU7AVa2VTRg/sendMessage?" . http_build_query($data));

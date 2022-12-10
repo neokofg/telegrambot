bb99559696787f9006be5c2326f78452ba3180e7
neoko <33611354+almasz243@users.noreply.github.com>
2022-12-10 19:34:53 +0300

    Update botcontroller.php

--- app/Http/Controllers/botcontroller.php
+++ app/Http/Controllers/botcontroller.php
@@ -274,7 +274,7 @@ class botcontroller extends Controller
                             'weight' => $update->message->text
                         ];
                         $validator = Validator::make($input, [
-                            'weight' => 'starts_with:integer|ends_with:кг'
+                            'weight' => 'integer|ends_with:кг'
                         ]);
                         if ($validator->fails()) {
                             $data = [

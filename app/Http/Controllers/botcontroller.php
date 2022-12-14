20540d3bf2ea6e04da8c4704d7ec58eda880ddc5
neoko <33611354+almasz243@users.noreply.github.com>
2022-12-14 19:02:08 +0300

    Update botcontroller.php

--- app/Http/Controllers/botcontroller.php
+++ app/Http/Controllers/botcontroller.php
@@ -138,7 +138,7 @@ class botcontroller extends Controller
                                 }';
                             $scopedecode = json_decode($scope);
                             $scopeencode = json_encode($scopedecode);
-                            $public_key = "-----BEGIN PUBLIC KEY-----
+                            $public_key = "-----BEGIN%20PUBLIC%20KEY-----
 MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA05FL3StCKstAZgOh4Bk1
 QEodBenu+BM1jwbYPWi0wyzLwrdglUgP3LnGQJk+jOoHaGtNbHJb5ejJZ7ETLkJY
 /dsmsi52+l2QE6CzosBPsbY1M3MUrVJvDUQZFWAs3BO+Y/2CimNNcGC0HQn1AEYO
@@ -146,7 +146,7 @@ soNrZN1GqdIjQlNCfvBoaqm8BvmkKEL3hiZPQfO0TUwPpLaf9ERHzIuYyVpyhroG
 sZ8jaN14br259ZVuQl9k1qMBX8/AqNvthjhI3mSc0vNquBDRUEFReLPO8ai/U9sm
 S8DSg/b50hcP56EA6fY1NK7Yhz4V4yeqeKU+vbxxDkhnN1aub10M/5Ay94cbJPUc
 eQIDAQAB
------END PUBLIC KEY-----";
+-----END%20PUBLIC%20KEY-----";
                             $userid = $userItem->userid;
                             $builddata = [
                                 'bot_id' => '5716304295',

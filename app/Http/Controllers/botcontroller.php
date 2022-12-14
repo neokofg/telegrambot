128db4a6d9f8cd63599b760babd6d93c83fd8b2c
neoko <33611354+almasz243@users.noreply.github.com>
2022-12-14 20:41:53 +0300

    Update botcontroller.php

--- app/Http/Controllers/botcontroller.php
+++ app/Http/Controllers/botcontroller.php
@@ -128,34 +128,6 @@ class botcontroller extends Controller
                                 "updated_at" => date('Y-m-d H:i:s')
                             );
                             DB::table('users')->where('userid','=',$update->message->from->id)->update($userdata);
-                            $scope =
-                                '{
-                                    "data": [[
-                                        {
-                                            "type": "address"
-                                        }]
-                                    ],
-                                    "v": "1"
-                                }';
-                            $scopedecode = json_decode($scope);
-                            $scopeencode = json_encode($scopedecode);
-                            $public_key = '-----BEGIN PUBLIC KEY-----
-MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA05FL3StCKstAZgOh4Bk1
-QEodBenu+BM1jwbYPWi0wyzLwrdglUgP3LnGQJk+jOoHaGtNbHJb5ejJZ7ETLkJY
-/dsmsi52+l2QE6CzosBPsbY1M3MUrVJvDUQZFWAs3BO+Y/2CimNNcGC0HQn1AEYO
-soNrZN1GqdIjQlNCfvBoaqm8BvmkKEL3hiZPQfO0TUwPpLaf9ERHzIuYyVpyhroG
-sZ8jaN14br259ZVuQl9k1qMBX8/AqNvthjhI3mSc0vNquBDRUEFReLPO8ai/U9sm
-S8DSg/b50hcP56EA6fY1NK7Yhz4V4yeqeKU+vbxxDkhnN1aub10M/5Ay94cbJPUc
-eQIDAQAB
------END PUBLIC KEY-----';
-                            $userid = $userItem->userid;
-                            $builddata = [
-                                'domain' => 'telegrampassport',
-                                'bot_id' => '5716304295',
-                                'scope' => $scopeencode,
-                                'public_key' => json_encode($public_key),
-                                'nonce' => rand(0,999999)
-                            ];
                             $keyboard =
                                 '{
                                     "inline_keyboard": [[

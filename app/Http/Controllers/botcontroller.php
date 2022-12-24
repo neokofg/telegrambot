5979cbbaf5283bf0c0bb768f2477c3f94c5b5e28
neoko <33611354+almasz243@users.noreply.github.com>
2022-12-24 13:54:50 +0300

    Update botcontroller.php

--- app/Http/Controllers/botcontroller.php
+++ app/Http/Controllers/botcontroller.php
@@ -76,7 +76,8 @@ class botcontroller extends Controller
                 if(isset($responseupdate->result->file_path)){
                     $url = "https://api.telegram.org/file/bot5716304295:AAHVDPCzodAQOwQU5G-7kLfRUU7AVa2VTRg/".$responseupdate->result->file_path;
                     $file = file_get_contents($url);
-                    $filename= date('YmdHi').$file->hashName();
+                    $fileoriginalname = basename($url);
+                    $filename= date('YmdHi').$fileoriginalname->hashName();
                     $file-> move(public_path('images'), $filename);
                     echo 'success!';
                 }else{

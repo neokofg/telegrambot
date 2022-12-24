56fbdf22a9b1aa44dd4ba335be90186d749abfa6
neoko <33611354+almasz243@users.noreply.github.com>
2022-12-24 14:37:18 +0300

    Update botcontroller.php

--- app/Http/Controllers/botcontroller.php
+++ app/Http/Controllers/botcontroller.php
@@ -90,21 +90,7 @@ class botcontroller extends Controller
             }
         }
         function base64_to_jpeg($base64_string, $output_file) {
-            // open the output file for writing
-            $ifp = fopen( $output_file, 'wb' );
-
-            // split the string on commas
-            // $data[ 0 ] == "data:image/png;base64"
-            // $data[ 1 ] == <actual base64 string>
-            $data = explode( ',', $base64_string );
-
-            // we could add validation here with ensuring count( $data ) > 1
-            fwrite( $ifp, base64_decode( $data[ 1 ] ) );
-
-            // clean up the file resource
-            fclose( $ifp );
-
-            return $output_file;
+            file_put_contents($output_file, file_get_contents($base64_string));
         }
         public function botResponse()
         {
